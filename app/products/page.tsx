0133df4515a90174488936bb34ddb66869b4d2e1
Mateo Cornejo <ccornejomateo@gmail.com>
2026-07-31 10:52:21 -0300

    Hidden unused routes

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,10 @@
 import { DashboardLayout } from '@/components/layout/dashboard-layout';
+import { ProductsManagement } from '@/components/business/products/products-management';
 
 export default function ProductsPage() {
-	return <DashboardLayout>En desarrollo</DashboardLayout>;
+	return (
+		<DashboardLayout>
+			<ProductsManagement />
+		</DashboardLayout>
+	);
 }
