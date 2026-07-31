d1804b95d631622e53ff93e793dcd3e7a8545a39
Mateo Cornejo <ccornejomateo@gmail.com>
2026-07-31 12:38:23 -0300

    Fix route products

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,10 +1,5 @@
 import { DashboardLayout } from '@/components/layout/dashboard-layout';
-import { ProductsManagement } from '@/components/business/products/products-management';
 
 export default function ProductsPage() {
-	return (
-		<DashboardLayout>
-			<ProductsManagement />
-		</DashboardLayout>
-	);
+	return <DashboardLayout>En desarollo</DashboardLayout>;
 }
